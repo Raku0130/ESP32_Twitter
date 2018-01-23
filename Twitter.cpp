d5391a5a51f7f2c9e6ed1b0b75d2b82e84c589ac
Raku0130 <29605712+Raku0130@users.noreply.github.com>
2018-01-24 00:53:15 +0900

    Add files via upload

---
+++ Twitter.cpp
@@ -0,0 +1,32 @@
+#include "Twitter.h"
+
+
+
+Twitter::Twitter(String _APIkey)
+{
+  APIkey = _APIkey;
+}
+
+void Twitter::tweet(String tsData)
+{
+  if (_client.connect("api.thingspeak.com", 80))
+  { 
+    // Create HTTP POST Data
+    tsData = "api_key="+APIkey+"&status="+tsData;
+            
+    _client.print("POST /apps/thingtweet/1/statuses/update HTTP/1.1\n");
+    _client.print("Host: api.thingspeak.com\n");
+    _client.print("Connection: close\n");
+    _client.print("Content-Type: application/x-www-form-urlencoded\n");
+    _client.print("Content-Length: ");
+    _client.print(tsData.length());
+    _client.print("\n\n");
+
+    _client.print(tsData);
+    
+  }
+  else
+  {
+    Serial.println("ERROR");
+  }
+}
---
+++ Twitter.h
@@ -0,0 +1,17 @@
+#ifndef _Twitter_h
+#define _Twitter_h
+
+#include "WiFi.h"
+
+class Twitter
+{
+  public :
+    Twitter(String _APIkey);
+    void tweet(String tsData);
+  private :
+    String APIkey;
+    WiFiClient _client;
+};
+
+
+#endif
